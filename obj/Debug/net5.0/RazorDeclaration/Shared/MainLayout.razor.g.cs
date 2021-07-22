4943c22a0bf32e1d061e0e99c24ccbb0baefa599
Joseph Mignone <jmig3066@gmail.com>
2021-07-22 12:48:21 -0400

    Finished Calculator example

--- obj/Debug/net5.0/RazorDeclaration/Shared/MainLayout.razor.g.cs
+++ obj/Debug/net5.0/RazorDeclaration/Shared/MainLayout.razor.g.cs
@@ -13,70 +13,70 @@ namespace PriceCalcHome.Shared
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "C:\Users\jmig2\Documents\GitHub\PriceCalc_Home\PriceCalcHome\_Imports.razor"
+#line 1 "C:\Users\jmig2\Documents\GitHub\PriceCalc_Home\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\jmig2\Documents\GitHub\PriceCalc_Home\PriceCalcHome\_Imports.razor"
+#line 2 "C:\Users\jmig2\Documents\GitHub\PriceCalc_Home\_Imports.razor"
 using System.Net.Http.Json;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\jmig2\Documents\GitHub\PriceCalc_Home\PriceCalcHome\_Imports.razor"
+#line 3 "C:\Users\jmig2\Documents\GitHub\PriceCalc_Home\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\jmig2\Documents\GitHub\PriceCalc_Home\PriceCalcHome\_Imports.razor"
+#line 4 "C:\Users\jmig2\Documents\GitHub\PriceCalc_Home\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\jmig2\Documents\GitHub\PriceCalc_Home\PriceCalcHome\_Imports.razor"
+#line 5 "C:\Users\jmig2\Documents\GitHub\PriceCalc_Home\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\jmig2\Documents\GitHub\PriceCalc_Home\PriceCalcHome\_Imports.razor"
+#line 6 "C:\Users\jmig2\Documents\GitHub\PriceCalc_Home\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\jmig2\Documents\GitHub\PriceCalc_Home\PriceCalcHome\_Imports.razor"
+#line 7 "C:\Users\jmig2\Documents\GitHub\PriceCalc_Home\_Imports.razor"
 using Microsoft.AspNetCore.Components.WebAssembly.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\Users\jmig2\Documents\GitHub\PriceCalc_Home\PriceCalcHome\_Imports.razor"
+#line 8 "C:\Users\jmig2\Documents\GitHub\PriceCalc_Home\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\Users\jmig2\Documents\GitHub\PriceCalc_Home\PriceCalcHome\_Imports.razor"
+#line 9 "C:\Users\jmig2\Documents\GitHub\PriceCalc_Home\_Imports.razor"
 using PriceCalcHome;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "C:\Users\jmig2\Documents\GitHub\PriceCalc_Home\PriceCalcHome\_Imports.razor"
+#line 10 "C:\Users\jmig2\Documents\GitHub\PriceCalc_Home\_Imports.razor"
 using PriceCalcHome.Shared;
 
 #line default
